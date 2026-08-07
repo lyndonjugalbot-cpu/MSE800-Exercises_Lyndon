9e0c985eeb047996015b892ab7b36f892baa9d39
lyndonjugalbot-cpu <lyndon.jugalbot@gmail.com>
2026-08-08 10:06:26 +1200

    BMI Optimized
    Exercise BMI Calculator Optimize

---
+++ BMI-Calculator-Optimized/bmi.py
@@ -0,0 +1,52 @@
+def isfloat(n):
+  """ 
+  If string can be converted to floating number 
+  returns that number, otherwise returns false
+  """
+  try:
+    n=float(n)
+    return n;
+  except ValueError:
+     return False;
+
+def inputfloat(hint):
+  """ 
+  Prints hint and asks to enter number.
+  Repeats until decimal number is entered.
+  """
+  ret = False
+  while ret is False:
+    ret = isfloat(input(hint))
+    if ret is False:
+      print("Please enter number")
+  return ret 
+
+class BMIcalculator:
+  def getdata(self):
+    """
+    Get weight in kgs and height in cms.
+    Height is entered in cetimetres and stored in metres
+    """
+
+    self.w = inputfloat("Please enter your weight in kilograms:")
+    self.h = inputfloat("Please enter your height in centimetres:")/100
+
+  def calculate(self):
+    """
+    Calculate and return bmi
+    """
+
+    return self.w/(self.h ** 2)
+
+
+def main():
+  print("Hello, let's calculate your BMI.");
+  
+  calc = BMIcalculator()
+  print()
+  calc.getdata()
+  bmi=calc.calculate()
+  print(f"Your BMI is {bmi: .2f}")
+
+if __name__ == "__main__":
+    main()
